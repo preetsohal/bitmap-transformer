f20377ffef402b97dab2a9c6091ed550d976aa92
preetsohal <preetsohal@live.com>
2016-07-17 22:03:22 -0700

    made little changes into test file but having problem while executing

--- test/sample-test.js
+++ test/sample-test.js
@@ -3,22 +3,27 @@ const fs = require('fs');
 
 const invertBitmap = require('../lib/invertBitmap');
 const restoreBitmap = require('../lib/restoreBitmap');
+
 describe('hooks', function() {
-  var fileData = [];
-  var restoredFileData = [];
+ var fileData = [];
+ var restoredFileData = [];
 
-  before(function(done) {
-    fs.readFile('../test/palette-bitmap.bmp', function(err, data) {
-      fileData.push(data.toString());
-      if(err) console.log(err);
-it('should return an array', function(){
-        expect(Array.isArray(current)).to.be.true;
-      });
+ before(function(done) {
+   fs.readFile('../test/palette-bitmap.bmp', function(err, data) {
+     fileData.push(data.toString());
+     if(err) console.log(err);
+     done();
+   });
+ });
 
-    it('should return the same array', function(){
-      expect(expected.length).to.equal(current.length);
-      for (var i=0; i<expected.length; i++) {
-        expect(expected[i]).equal(current[i]);
-      }
-    })
-  })
+ it('should return an array', function(){
+   expect(Array.isArray(current));
+ });
+
+ it('should return the same array', function(){
+   expect(expected.length).to.eql(current.length);
+   for (var i=0; i<expected.length; i++) {
+     expect(expected[i]).to.eql(current[i]);
+   }
+ });
+}); 
